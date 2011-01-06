3c31791ae0e463984ac08807402e6e13a6fd34b5
Paul Annesley <paul@annesley.cc>
2011-01-06 19:33:12 +1100

    NativeSocket uses StreamFunctions methods, not PHP functions; mocked in NativeSocketTest.

--- tests/Pheanstalk/NativeSocketTest.php
+++ tests/Pheanstalk/NativeSocketTest.php
@@ -7,28 +7,46 @@
  * @package Pheanstalk
  * @licence http://www.opensource.org/licenses/mit-license.php
  */
-class Pheanstalk_ExceptionsTest
+class Pheanstalk_NativeSocketTest
 	extends UnitTestCase
 {
+	private $_streamFunctions;
+
+	public function setUp()
+	{
+		Mock::generate('Pheanstalk_Socket_StreamFunctions', 'MockStreamFunctions');
+
+		$instance = new MockStreamFunctions();
+		$instance->setReturnValue('fsockopen', true);
+
+		Pheanstalk_Socket_StreamFunctions::setInstance($instance);
+		$this->_streamFunctions = $instance;
+	}
+
+	public function tearDown()
+	{
+		Pheanstalk_Socket_StreamFunctions::unsetInstance();
+	}
+
 	public function testWrite()
 	{
-		$this->expectException('Pheanstalk_Exception_SocketException', 'Write should throw an exception if fwrite returns false');
-		$socket = new Test_NativeSocket();
-		$socket->write(1);
+		$this->_streamFunctions->setReturnValue('fwrite', false);
+
+		$this->expectException('Pheanstalk_Exception_SocketException',
+			'Write should throw an exception if fwrite returns false');
+
+		$socket = new Pheanstalk_Socket_NativeSocket('host', 1024, 0);
+		$socket->write('data');
 	}
 
 	public function testRead()
 	{
-		$this->expectException('Pheanstalk_Exception_SocketException', 'Read should throw an exception if fread returns false');
-		$socket = new Test_NativeSocket();
-		$socket->read(1);
-	}
-}
+		$this->_streamFunctions->setReturnValue('fread', false);
 
-class Test_NativeSocket extends Pheanstalk_Socket_NativeSocket
-{
-	public function __construct()
-	{
-		$this->_socket = NULL;
+		$this->expectException('Pheanstalk_Exception_SocketException',
+			'Read should throw an exception if fread returns false');
+
+		$socket = new Pheanstalk_Socket_NativeSocket('host', 1024, 0);
+		$socket->read(1);
 	}
 }
