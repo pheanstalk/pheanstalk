3ecd6738b70638a021423ec3b2eb31fdf4723799
Josh Butts <josh@joshbutts.com>
2012-02-07 21:40:47 -0600

    fix bugs in phar stub process

--- makephar.php
+++ makephar.php
@@ -4,12 +4,11 @@
     $phar = new Phar(__DIR__ . '/pheanstalk.phar');
     $phar->buildFromDirectory(__DIR__ . '/classes');
 
-    $bootstrapFile = file_get_contents(__DIR__ . '/pheanstalk_init.php');
-    $bootstrapFile = str_replace('<?php', '', $bootstrapFile);
-
     $stub .= "<?php" . PHP_EOL;
     $stub .= "Phar::mapPhar();" . PHP_EOL;
-    $stub .= $bootstrapFile . PHP_EOL;
+    $stub .= '$pheanstalkClassRoot = "phar://" . __FILE__;';
+	$stub .= 'require_once $pheanstalkClassRoot . "/Pheanstalk/ClassLoader.php";';
+	$stub .= 'Pheanstalk_ClassLoader::register($pheanstalkClassRoot);';
     $stub .= "__HALT_COMPILER();" . PHP_EOL;
     $phar->setStub($stub);
 ?>
