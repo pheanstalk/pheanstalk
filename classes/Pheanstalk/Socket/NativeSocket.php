6251b3babc6987d5dd006a249a4d52c6bc8032dd
mdave <mdave@wittybee.net>
2016-01-27 16:14:18 +0530

    Fix : infinite loop in case  of remote beanstalk server goes unreachable from network
    (cherry picked from commit 7bc2ccc8c24545eee3e08f0b51d56f0eeee99969)

--- classes/Pheanstalk/Socket/NativeSocket.php
+++ classes/Pheanstalk/Socket/NativeSocket.php
@@ -93,6 +93,18 @@ class Pheanstalk_Socket_NativeSocket implements Pheanstalk_Socket
             $data = isset($length) ?
                 $this->_wrapper()->fgets($this->_socket, $length) :
                 $this->_wrapper()->fgets($this->_socket);
+                
+               if($data === false) {
+				$info = $this->_wrapper()->stream_get_meta_data( $this->_socket );
+
+				//if time out occured and there are no unread bytes
+				//we might have socket connection to server lost
+				//a particualr case when remove host is no longer rechable
+				if ($info ['timed_out'] && $info['unread_bytes']==0) {
+					throw new Exception\SocketException ( "Socket connection lost" );
+				}
+			}
+ 
 
             if ($this->_wrapper()->feof($this->_socket)) {
                 throw new Pheanstalk_Exception_SocketException("Socket closed by server!");
