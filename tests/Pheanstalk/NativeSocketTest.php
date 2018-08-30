53cf5b977ab14137a494abd4834ab41bdeba34ca
Sam Mousa <sam@mousa.nl>
2018-08-30 15:52:33 +0200

    Test for timeout as well as repetitions

--- tests/Pheanstalk/NativeSocketTest.php
+++ tests/Pheanstalk/NativeSocketTest.php
@@ -79,19 +79,29 @@ class NativeSocketTest extends \PHPUnit_Framework_TestCase
     public function testGetLine()
     {
         // Set the timeout.
-        ini_set('default_socket_timeout', 10);
+        ini_set('default_socket_timeout', $timeout = 5);
         /**
-         * We're trying to test for an infinite loop, instead we limit it to 10.000
+         * We're trying to test for an infinite loop, since we can't do this we instead limit the number of invocations.
          */
-        $this->_streamFunctions->expects($this->atMost(15))
-            ->method('fread')
+        $this->_streamFunctions->expects($this->atMost(1000))
+            ->method('fgets')
             ->will($this->returnCallback(function() {
-                sleep(1);
+                static $counter = 0;
+                $counter++;
+                usleep(5000);
+                if ($counter > 2000) {
+                    return '';
+                }
                 return false;
             }));
 
         $socket = new NativeSocket(self::DEFAULT_HOST, self::DEFAULT_HOST, self::DEFAULT_CONNECTION_TIMEOUT, self::DEFAULT_PERSISTENT_CONNECTION);
+
+        $start = microtime(true);
         $socket->getLine();
+        // Check that get line takes no more than timeout + 1 seconds;
+        // this is reasonable based on the fact that we only sleep for 1/20 of a second on each iteration.
+        $this->assertLessThan(5 + 1, microtime(true) - $start, 'getLine did not respect the configured timeout');
     }
 
 
