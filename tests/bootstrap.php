6adb2c05c1a4616068a6b32f9ae8564ff9195cbb
Sam Mousa <sam@mousa.nl>
2021-12-22 16:42:38 +0100

    ci: use normal runner

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-define('SERVER_HOST', is_string(getenv('SERVER_HOST')) ? getenv('SERVER_HOST') : 'beanstalk');
+define('SERVER_HOST', is_string(getenv('SERVER_HOST')) ? getenv('SERVER_HOST') : 'localhost');
 const SERVER_PORT = 11300;
