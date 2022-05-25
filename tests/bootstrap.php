fe4a966ef444a151af644bc4686746e8103495c9
Sam Mousa <sam@mousa.nl>
2022-05-25 13:04:32 +0200

    ci: test on unix-socket

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 define('SERVER_HOST', is_string(getenv('SERVER_HOST')) ? getenv('SERVER_HOST') : 'localhost');
-const SERVER_PORT = 11300;
-const UNIX_SERVER_HOST = 'unix:///shared/beanstalkd.sock';
+define('UNIX_SERVER_HOST', is_string(getenv('UNIX_SERVER_HOST')) ? getenv('UNIX_SERVER_HOST') : 'unix:///shared/beanstalkd.sock');
